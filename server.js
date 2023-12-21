b4ba3fed24e16f54258d43e102e3f6d8a69adefd
Roman Larionov <pcsaveddotcom@gmail.com>
2023-12-20 21:52:56 -0500

    show page added

--- server.js
+++ server.js
@@ -63,7 +63,14 @@ app.post("/books", async (req, res) => {
 })
 
 //Show - GET rendering only one book
-
+app.get("/books/:id", async (req, res) => {
+    // find a book by _id
+    let foundBook = await Book.findById(req.params.id) // the request params object
+    // render show.ejs with the foundBook
+    res.render("show.ejs", {
+        book: foundBook
+    })
+})
 
 
 app.listen(PORT, () => {
