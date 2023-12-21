3625ba6c33f1eb8afedc13dcbbcb62fb402956ff
Roman Larionov <pcsaveddotcom@gmail.com>
2023-12-20 19:55:07 -0500

    server and book created and tested

---
+++ server.js
@@ -0,0 +1,56 @@
+//  R'N'R   December 20, 2023
+
+//DEPENDENCIES
+require("dotenv").config(); //using our .env variable
+
+require("./config/db")  //bring our db connection
+
+const express = require("express");
+const morgan = require("morgan");
+
+
+const app = express();
+
+const { PORT = 3013 } = process.env;
+
+//importing Book model
+const Book = require("./models/book");
+
+
+
+//MIDDLEWARE
+app.use(morgan("dev")) // logging
+app.use(express.urlencoded({ extended: true })) // body parser this is how we get access to req.body
+
+
+//ROUTES
+
+//Index - GET render all of the books
+app.get("/", (req, res) => {
+    res.send(`It is alive`)
+})
+
+//New - GET for the form to create a new book
+
+//Create-POST
+app.post("/books", async (req, res) => {
+    //res.send(req.body)    //initial setting
+    if (req.body.completed === "on") {
+        // if checked
+        req.body.completed = true
+    } else {
+        // if not checked
+        req.body.completed = false
+    }
+    //res.send(req.body)
+    let newBook = await Book.create(req.body)
+    res.send(newBook)
+})
+
+//Show - GET rendering only one book
+
+
+
+app.listen(PORT, () => {
+    console.log(`I am alive on port ${PORT}`);
+})
